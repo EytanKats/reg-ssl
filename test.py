6546da19d7cf55dca13c67ce51fd0ef529923c9b
EytanKats <eytan.kats@gmail.com>
2024-07-29 15:23:58 +0200

    print sdlogj on test

--- test.py
+++ test.py
@@ -11,5 +11,6 @@ def test(args):
     data = prepare_data(data_split='test')
     feature_net = torch.load(ckpt_path).cuda()
 
-    all_fields, d_all_net, d_all0, d_all_adam, d_all_ident = update_fields(data, feature_net, True, num_warps=2, compute_jacobian=True, ice=True, reg_fac=10.)
+    all_fields, d_all_net, d_all0, d_all_adam, d_all_ident, sdlogj, sdloj_adam = update_fields(data, feature_net, True, num_warps=2, compute_jacobian=True, ice=True, reg_fac=10.)
     print('DSC:', d_all0.sum() / (d_all_ident > 0.1).sum(), '>', d_all_net.sum() / (d_all_ident > 0.1).sum(), '>', d_all_adam.sum() / (d_all_ident > 0.1).sum())
+    print('SDLJ:', sdlogj, '>', sdloj_adam)
