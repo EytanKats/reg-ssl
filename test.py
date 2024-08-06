0a29a691b85aad36205eea6c412b33706cbe5d0f
EytanKats <eytan.kats@gmail.com>
2024-08-06 10:19:28 +0200

    choose data in test script according to configuration

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import torch
 
-from data_utils import prepare_data
+from data_utils import prepare_radchest_data, prepare_abdomenctct_data
 from registration_pipeline import update_fields
 
 
@@ -8,7 +8,11 @@ def test(args):
 
     ckpt_path = args.ckpt_path
 
-    data = prepare_data(data_split='test')
+    if args.dataset == 'abdomenctct':
+        data = prepare_abdomenctct_data(data_split='test')
+    elif args.dataset == 'radchestct':
+        data = prepare_radchest_data(data_split='test')
+
     feature_net = torch.load(ckpt_path).cuda()
 
     all_fields, d_all_net, d_all0, d_all_adam, d_all_ident, sdlogj, sdloj_adam = update_fields(data, feature_net, True, num_warps=2, compute_jacobian=True, ice=True, reg_fac=10.)
